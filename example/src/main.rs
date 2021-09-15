cf8ee63923489016b6a8f1d474d2e9e9c2c6aa63
lzw <657434763@qq.com>
2021-09-15 13:10:26 +0800

    fix

--- example/src/main.rs
+++ example/src/main.rs
@@ -1,6 +1,7 @@
-
+use std::collections::HashMap;
 fn main() {
 
+    // ///结构体
     // #[derive(Debug,Clone)]
     // struct student{
     //     name: String,
@@ -15,13 +16,14 @@ fn main() {
     //     });
     // }
     //
-    //
+    // ///map
     // let x = students.clone().into_iter().map(|mut x1| {
     //     x1.id += 1;
     //     x1
     // }).collect::<Vec<_>>();
     // println!("{:?}",x);
     //
+    // ///filter
     // let vec1 = students.into_iter().filter(| x2| {
     //     if x2.id == 2 {
     //         true
@@ -30,25 +32,32 @@ fn main() {
     //     }
     // }).collect::<Vec<_>>();
     // println!("{:?}",vec1);
-
-
-
-
-    // 排序
+    //
+    // ///sort
     // let mut vec1 = vec![1, 50, 60, 4, 10, 9, 11, 20, 30];
     // vec1.sort_by(|a, b| b.cmp(&a));
     // println!("{:?}", vec1);
-
-
+    //
+    // ///二分查找
     // match vec1.binary_search(&7) {
     //     Ok(a) => print!("{}",a),
     //     Err(s) => print!("{}",s)
     // }
 
 
-    let s1 = String::from("qqq");
-    let s2 = s1;
-    println!("{:?}", s1.as_ptr());
-    println!("{:?}", s2.as_ptr());
+    ///hashmap
+    let mut map = HashMap::new();
+    map.insert(String::from("lzw"),10);
+    map.insert(String::from("lzww"),100);
+
+    let value = map.get("lzw");
+    println!("{:?}", value.unwrap());
+
+
 }
 
+#[test]
+fn test1(){
+    let a = 1;
+    assert_eq!(a,1)
+}
